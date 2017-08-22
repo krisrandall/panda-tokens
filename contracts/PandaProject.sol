2024ad8deec993c1ca706d2f9620dea86f1bf985
bazmatic <barryearsman@gmail.com>
2017-08-22 12:37:20 +1000

    Added initial contracts for PandaToken and PandaProject

---
+++ contracts/PandaProject.sol
@@ -0,0 +1,100 @@
+pragma solidity ^0.4.15;
+import "./PandaToken.sol"; 
+
+
+contract PandaProject {
+
+    bytes32 public name;
+    address[] public owners;
+    PandaToken public pandaTokenContract;
+
+    struct Distribution {
+        address recipient;
+        uint256 share;
+    }
+
+    struct Donation {
+        address donor;
+        uint256 amount;
+    }
+
+    Distribution[5] public distributions;
+
+    function PandaProject(bytes32 projectName, address[5] recipientAddresses, uint256[5] recipientShares, address pandaTokenContractAddr) 
+    {
+        owners.push(msg.sender);
+        name = projectName;
+        pandaTokenContract = PandaToken(pandaTokenContractAddr);
+        for (uint i=0; i < recipientAddresses.length; i++) {
+            distributions[i] = Distribution({
+                recipient: recipientAddresses[i],
+                share: recipientShares[i]
+            });
+        }
+    }
+
+    /// @notice Donate panda tokens to this project. Tokens are distributed among recipients
+    function donate(uint256 totalAmount) {
+        Distribution memory distribution;
+        uint256 remainingAmount;
+        remainingAmount = totalAmount;
+
+        for(uint i = 0; i < distributions.length; i++) {
+            distribution = distributions[i];
+            uint256 distAmount = totalAmount * distribution.share / 100;
+            remainingAmount -= distAmount;
+            pandaTokenContract.sendTokens(distAmount, distribution.recipient); //Send tokens from sender to recipient
+        }
+        //If any remaining, give to random recipient
+        //TODO: Make random. Using first recipient as placeholder
+        pandaTokenContract.sendTokens(remainingAmount, distributions[0].recipient);
+    }
+       
+    
+    modifier onlyOwner() {
+        if (isOwner(msg.sender)) 
+            _;
+    }   
+
+    function kill() onlyOwner {
+        suicide(msg.sender);
+    }
+    
+
+    function isOwner(address owner) returns (bool) {
+        uint i = 0;
+        bool found = false;
+        for (i=0; i<owners.length; i++) {
+            if (owners[i] == owner) {
+                found = true;
+                break;
+            }
+        }
+        return found;
+    }
+
+    function addOwner(address newOwner) onlyOwner {
+       owners.push(newOwner); 
+    }
+    
+    function removeOwner(address ownerToRemove) onlyOwner {
+        if (owners.length == 1) {
+            revert(); //Can't remove all owners
+        }
+        else {
+            uint i = 0;
+            for (i=0; i<owners.length; i++) {
+                if (owners[i] == ownerToRemove) {
+                    delete owners[i];
+                    break;
+                }
+            }
+        }          
+    } 
+
+    
+    //Sending ether to this contract is not allowed
+    function () {
+        revert();
+    }
+}
---
+++ contracts/PandaToken.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.4.15;
+contract PandaToken { 
+
+    mapping (address => uint256) public balances;
+    address public owner;
+
+    function PandaToken() {
+        owner = msg.sender;
+    }
+
+    ///  @notice Give free tokens to the sender
+    function tokenFaucet(uint256 amount) public returns (uint256) {
+        balances[msg.sender] += amount;
+        return balances[msg.sender];
+    }
+
+    /// @notice Send tokens from the sender address to another address
+    /// @param amount Amount of tokens to send
+    /// @param destination Account to receive tokens
+    /// @return Remaining balance of sender
+    function sendTokens(uint256 amount, address destination) public returns (uint256 remainingBal) {
+        if(balances[msg.sender] >= amount) {
+            balances[msg.sender] -= amount;
+            balances[destination] += amount;           
+        }
+
+        return balances[msg.sender];
+    }
+
+    /// @notice Get balance
+    /// @return The balance of the sender
+    function balance() constant returns (uint256) {
+        return balances[msg.sender];
+    }
+
+    function () {
+        revert();
+    }
+}
