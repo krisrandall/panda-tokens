466db51c5c8297001f5cd1d70d147a311584108f
Kris <kris@Kriss-MacBook-Pro.local>
2017-08-22 11:20:15 +1000

    add Ropsten testnet, I can now get this running with the following steps: 1. in one console : $ eth --fast --cache=1048 --testnet --rpc --rpcapi "eth,net,web3" --rpccorsdomain '*' --rpcaddr localhost --rpcport 8545 --unlock 0x4Fcb8b0a22461BB32A7B95Cd7D0dB64fd2767433 2. in another console :  $ truffle compile  $ truffle migrate --network ropsten  $ npm run dev It currently runs the truffle metacoin demo, so now I move my stuff over ..

--- truffle.js
+++ truffle.js
@@ -3,10 +3,15 @@ require('babel-register')
 
 module.exports = {
   networks: {
-    development: {
-      host: 'localhost',
+    localhost: {
+      host: "localhost", 
+      port: 8546,
+      network_id: "*" 
+    },  
+    ropsten: {
+      host: "localhost",
       port: 8545,
-      network_id: '*' // Match any network id
+      network_id: "3"
     }
   }
-}
+};
