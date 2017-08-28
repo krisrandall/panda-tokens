8ef980caea10ffb299f645bc73fc068188b1bcdb
Kris <kris@Kriss-MacBook-Pro.local>
2017-08-28 13:00:28 +1000

    fixes to pandatoken contract, as well redeploy

--- app/javascripts/config.js
+++ app/javascripts/config.js
@@ -1,8 +1,8 @@
 
 export default {
     contract_locations : {
-        panda_token   : '0x6d96B021fF0BA0Fe0D28B477De1b3036AFB28eB2',
-        panda_project : '0xD26BC60a56f4e9a260D8Ad2b9B2B0A1A6783Fed2'
+        panda_token   : '0x61132549aECEa450d03FefdbE6489B2503F5b998',
+        panda_project : '0x9494BEcEd1326C0841Ed36b0C0Ca5699BaDb4420'
     },
 
     recepient_descriptions : {
