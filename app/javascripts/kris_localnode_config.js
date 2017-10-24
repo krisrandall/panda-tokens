4ccff2079f0276f98b3bbef4c55f8f884c17c3f2
krisrandall <kris.randall@gmail.com>
2017-10-24 15:04:54 +1000

    save my local node config in another config file

---
+++ app/javascripts/kris_localnode_config.js
@@ -0,0 +1,14 @@
+
+export default {
+    contract_locations : {
+        panda_token   : '0x2d6f6e5f73f6f0b1f116104d453a5ed186176bd2',
+        panda_project : '0xdb1c7318635d42233210432c924fea4bc1526ee0'
+    },
+
+    recepient_descriptions : {
+        "0xed9e87f469a976167e9eccc0b6dd2268f2053daf": { name: "Panda Rescue  for rescuers training", col: "rgb(255, 191, 0)" },
+        "0xc0709427cb65984a4e4e80a17aa74811c6dddc85": { name: "Panda Habitat for habitat conservation", col: "silver" },
+        "0x664d70f2156fe0d3cf7dccb9c0b1a1586c28602a": { name: "Panda Token for appeal administation", col: "rgb(55, 113, 202)" }
+        /* Note that these colour values are also in the app.css .ct-slice-donut */
+    }
+}
